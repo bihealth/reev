ee5ab62050edff5c02cec9c735908f5e653b9295
gromdimon <grom.dima.grom@gmail.com>
2023-08-15 12:28:10 +0200

    feat: environment files

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,24 @@
 import os
 import sys
 
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import RedirectResponse
 
+# Load environment
+env = os.environ
+load_dotenv()
+
 #: Path to frontend build, if any.
-SERVE_FRONTEND = os.environ.get("REEV_SERVE_FRONTEND")
+SERVE_FRONTEND = env.get("REEV_SERVE_FRONTEND")
+#: Debug mode
+DEBUG = env.get("REEV_DEBUG", "false").lower() in ("true", "1")
+#: Prefixes for varfish-docker-compose-ng
+BACKEND_PREFIX_MEHARI = env.get("REEV_BACKEND_PREFIX_MEHARI", "http://mehari")
+BACKEND_PREFIX_VIGUNO = env.get("REEV_BACKEND_PREFIX_VIGUNO", "http://viguno")
+BACKEND_PREFIX_ANNONARS = env.get("REEV_BACKEND_PREFIX_ANNONARS", "http://annonars")
+
 
 app = FastAPI()
 
