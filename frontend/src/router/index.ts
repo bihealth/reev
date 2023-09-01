6d6c69ea6255a67c7a309116da49ce8bbb4a1f20
gromdimon <grom.dima.grom@gmail.com>
2023-09-01 12:38:37 +0200

    fix: lint _from in router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -41,7 +41,7 @@ const router = createRouter({
 /** The default page title */
 const DEFAULT_TITLE = 'REEV Explains and Evaluates Variants'
 
-router.afterEach((to, _from) => {
+router.afterEach((to) => {
   // Use next tick to handle router history correctly
   // see: https://github.com/vuejs/vue-router/issues/914#issuecomment-384477609
   nextTick(() => {
