2e96f42ecbf5fd6ac0d3adff57a9c0ce7b6f86d2
gromdimon <grom.dima.grom@gmail.com>
2023-09-05 08:58:10 +0200

    feat: search results vue

--- frontend/src/views/GenesListView.vue
+++ frontend/src/views/GenesListView.vue
@@ -33,13 +33,83 @@ watch(() => router.currentRoute.value.query, loadDataToStore)
 //     }
 //   }
 // )
+const exampleData = {
+  genes: [
+    {
+      score: 0.8,
+      data: {
+        hgnc_id: 'HGNC:1100',
+        symbol: 'BRCA1',
+        name: 'BRCA1 DNA repair associated',
+        alias_symbol: ['RNF53', 'BRCC1', 'PPP1R53', 'FANCS'],
+        alias_name: [
+          'BRCA1/BRCA2-containing complex, subunit 1',
+          'protein phosphatase 1, regulatory subunit 53',
+          'Fanconi anemia, complementation group S'
+        ],
+        ensembl_gene_id: 'ENSG00000012048',
+        ncbi_gene_id: '672'
+      }
+    },
+    {
+      score: 0.8,
+      data: {
+        hgnc_id: 'HGNC:1101',
+        symbol: 'BRCA2',
+        name: 'BRCA2 DNA repair associated',
+        alias_symbol: ['FAD', 'FAD1', 'BRCC2', 'XRCC11'],
+        alias_name: ['BRCA1/BRCA2-containing complex, subunit 2'],
+        ensembl_gene_id: 'ENSG00000139618',
+        ncbi_gene_id: '675'
+      }
+    },
+    {
+      score: 0.5714286,
+      data: {
+        hgnc_id: 'HGNC:28470',
+        symbol: 'BRCA1P1',
+        name: 'BRCA1 pseudogene 1',
+        alias_symbol: ['LBRCA1', 'PsiBRCA1', 'pseudo-BRCA1'],
+        alias_name: ['like-BRCA1'],
+        ensembl_gene_id: 'ENSG00000267595',
+        ncbi_gene_id: '394269'
+      }
+    }
+  ]
+}
 </script>
 
 <template>
   <HeaderDetailPage />
   <v-container class="about-view">
     <div v-if="genesListStore.storeState == StoreState.Active">
-      <h2>HGNC</h2>
+      <h2>Search results:</h2>
+
+      <div v-for="gene in genesListStore.genesList" :key="gene.data.hgnc_id">
+        <v-card class="gene-item">
+          <v-card-title>
+            <h2>{{ gene.data.symbol }}</h2>
+            <router-link
+              :to="{
+                name: 'gene',
+                params: { searchTerm: gene.data.hgnc_id, genomeRelease: 'grch37' }
+              }"
+            >
+              {{ gene.data.hgnc_id }} <v-icon>mdi-launch</v-icon>
+            </router-link>
+          </v-card-title>
+          <v-divider />
+          <v-card-text>
+            <div><strong>Name:</strong> {{ gene.data?.name }}</div>
+            <div><strong>Symbol:</strong> {{ gene.data?.symbol }}</div>
+            <div><strong>Full name:</strong> {{ gene.data?.name }}</div>
+            <div><strong>Alias symbol:</strong> {{ gene.data?.alias_symbol }}</div>
+            <div><strong>Alias name:</strong> {{ gene.data?.alias_name }}</div>
+            <div><strong>Ensembl id:</strong> {{ gene.data?.ensembl_gene_id }}</div>
+            <div><strong>NCBI id:</strong> {{ gene.data?.ncbi_gene_id }}</div>
+          </v-card-text>
+        </v-card>
+      </div>
     </div>
     <div v-else-if="genesListStore.storeState == StoreState.Loading">
       <div class="d-flex align-center justify-center" style="min-height: 300px">
@@ -54,6 +124,43 @@ watch(() => router.currentRoute.value.query, loadDataToStore)
       </h2>
       <h2>Searched query:</h2>
       {{ $route.query }}
+      <v-divider />
+      <h2>Example data:</h2>
+
+      <div v-for="gene in exampleData.genes" :key="gene.data.hgnc_id">
+        <v-card class="gene-item">
+          <v-card-title>
+            <h2>{{ gene.data.symbol }}</h2>
+            <router-link
+              :to="{
+                name: 'gene',
+                params: { searchTerm: gene.data.hgnc_id, genomeRelease: 'grch37' }
+              }"
+            >
+              {{ gene.data.hgnc_id }} <v-icon>mdi-launch</v-icon>
+            </router-link>
+          </v-card-title>
+          <v-divider />
+          <v-card-text>
+            <div><strong>Name:</strong> {{ gene.data.name }}</div>
+            <div><strong>Symbol:</strong> {{ gene.data.symbol }}</div>
+            <div><strong>Full name:</strong> {{ gene.data.name }}</div>
+            <div><strong>Alias symbol:</strong> {{ gene.data.alias_symbol }}</div>
+            <div><strong>Alias name:</strong> {{ gene.data.alias_name }}</div>
+            <div><strong>Ensembl id:</strong> {{ gene.data.ensembl_gene_id }}</div>
+            <div><strong>NCBI id:</strong> {{ gene.data.ncbi_gene_id }}</div>
+          </v-card-text>
+        </v-card>
+      </div>
     </div>
   </v-container>
 </template>
+
+<style scoped>
+.gene-item {
+  margin-bottom: 20px;
+  border: 2px solid rgb(229, 85, 64);
+  border-radius: 10px;
+  padding: 5px 10px;
+}
+</style>
