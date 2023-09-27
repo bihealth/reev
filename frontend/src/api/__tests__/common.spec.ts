8a5d5770abd7eb1b49db50172bb7ee1b6fda60dc
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-09-27 16:09:34 +0200

    adjusting tests

--- frontend/src/api/__tests__/common.spec.ts
+++ frontend/src/api/__tests__/common.spec.ts
@@ -1,23 +1,29 @@
 import { describe, it, expect } from 'vitest'
 
-import { API_BASE_PREFIX, API_BASE_PREFIX_ANNONARS, API_BASE_PREFIX_MEHARI } from '../common'
+import { API_BASE_PREFIX, API_BASE_PREFIX_ANNONARS, API_BASE_PREFIX_MEHARI, API_BASE_PREFIX_NGINX } from '../common'
 
 describe.concurrent('API_BASE_PREFIX constants', () => {
   it('returns the correct API base prefix in production mode', () => {
     const originalMode = import.meta.env.MODE
-    expect(API_BASE_PREFIX).toBe('/')
+    expect(API_BASE_PREFIX).toBe('/internal/')
     import.meta.env.MODE = originalMode
   })
 
   it('returns the correct API base prefix for annonars in production mode', () => {
     const originalMode = import.meta.env.MODE
-    expect(API_BASE_PREFIX_ANNONARS).toBe('/proxy/annonars')
+    expect(API_BASE_PREFIX_ANNONARS).toBe('/internal/proxy/annonars')
     import.meta.env.MODE = originalMode
   })
 
   it('returns the correct API base prefix for mehari in production mode', () => {
     const originalMode = import.meta.env.MODE
-    expect(API_BASE_PREFIX_MEHARI).toBe('/proxy/mehari')
+    expect(API_BASE_PREFIX_MEHARI).toBe('/internal/proxy/mehari')
+    import.meta.env.MODE = originalMode
+  })
+
+  it('returns the correct API base prefix for nginx in production mode', () => {
+    const originalMode = import.meta.env.MODE
+    expect(API_BASE_PREFIX_NGINX).toBe('/internal/proxy/nginx')
     import.meta.env.MODE = originalMode
   })
 })
