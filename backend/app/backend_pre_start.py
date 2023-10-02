56b7c1afeea8e4d952e4f17f005efa693f08d760
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-10-02 07:45:06 +0200

    fix: database credential access in Docker build

--- backend/app/backend_pre_start.py
+++ backend/app/backend_pre_start.py
@@ -1,9 +1,11 @@
+import asyncio
 import logging
 
 from sqlalchemy import text
 from tenacity import after_log, before_log, retry, stop_after_attempt, wait_fixed
 
-from app.db.session import SyncSessionLocal
+import alembic.config
+from app.db.session import SessionLocal
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -18,21 +20,24 @@ wait_seconds = 1
     before=before_log(logger, logging.INFO),
     after=after_log(logger, logging.WARN),
 )
-def init() -> None:
+async def init():
     try:
-        db = SyncSessionLocal()
+        db = SessionLocal()
         # Try to create session to check if DB is awake
-        db.execute(text("SELECT 1"))
+        await db.execute(text("SELECT 1"))
+        # Ensure to run Alembic on startup
+        alembicArgs = ["--raiseerr", "upgrade", "head"]
+        alembic.config.main(alembicArgs)
     except Exception as e:
         logger.error(e)
         raise e
 
 
-def main() -> None:
+async def main():
     logger.info("Initializing service")
-    init()
+    await init()
     logger.info("Service finished initializing")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
