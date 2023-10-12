15f72d91eb2b0ef4eb0da0a730098f97e54c2114
gromdimon <grom.dima.grom@gmail.com>
2023-10-12 13:51:40 +0200

    minor

--- backend/app/api/internal/endpoints/remote.py
+++ backend/app/api/internal/endpoints/remote.py
@@ -46,6 +46,27 @@ def default_acmg_rating() -> dict[str, bool]:
     return {k: False for k in ACMG_RATING_KEYS}
 
 
+class CustomHttpAdapter(requests.adapters.HTTPAdapter):
+    # "Transport adapter" that allows us to use custom ssl_context.
+
+    def __init__(self, ssl_context=None, **kwargs):
+        self.ssl_context = ssl_context
+        super().__init__(**kwargs)
+
+    def init_poolmanager(self, connections, maxsize, block=False):
+        self.poolmanager = urllib3.poolmanager.PoolManager(
+            num_pools=connections, maxsize=maxsize, block=block, ssl_context=self.ssl_context
+        )
+
+
+def get_legacy_session():
+    ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+    ctx.options |= 0x4  # OP_LEGACY_SERVER_CONNECT
+    session = requests.session()
+    session.mount("https://", CustomHttpAdapter(ctx))
+    return session
+
+
 router = APIRouter()
 
 
@@ -103,27 +124,6 @@ async def acmg(request: Request):
         if key.lower() in acmg_rating:
             acmg_rating[key.lower()] = value == 1
     return JSONResponse(acmg_rating)
-
-
-class CustomHttpAdapter(requests.adapters.HTTPAdapter):
-    # "Transport adapter" that allows us to use custom ssl_context.
-
-    def __init__(self, ssl_context=None, **kwargs):
-        self.ssl_context = ssl_context
-        super().__init__(**kwargs)
-
-    def init_poolmanager(self, connections, maxsize, block=False):
-        self.poolmanager = urllib3.poolmanager.PoolManager(
-            num_pools=connections, maxsize=maxsize, block=block, ssl_context=self.ssl_context
-        )
-
-
-def get_legacy_session():
-    ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
-    ctx.options |= 0x4  # OP_LEGACY_SERVER_CONNECT
-    session = requests.session()
-    session.mount("https://", CustomHttpAdapter(ctx))
-    return session
 
 
 @router.get("/cnv/acmg/{path:path}")
