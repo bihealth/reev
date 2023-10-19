cf1567f35ef68a7720e2e857a6a60ba1332da054
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-10-19 07:54:02 +0200

    wip

--- frontend/src/components/__tests__/VariantDetails/ClinVar.spec.ts
+++ frontend/src/components/__tests__/VariantDetails/ClinVar.spec.ts
@@ -3,19 +3,20 @@ import { describe, expect, it } from 'vitest'
 import ClinVar from '@/components/VariantDetails/ClinVar.vue'
 import { setupMountedComponents } from '@/lib/test-utils'
 
-const clinVarInfo =             {
-  "chrom": "17",
-  "pos": 41215920,
-  "reference": "G",
-  "alternative": "T",
-  "vcv": "VCV000055407",
-  "reference_assertions": [
+const clinVarInfo = {
+  chrom: '17',
+  pos: 41215920,
+  reference: 'G',
+  alternative: 'T',
+  vcv: 'VCV000055407',
+  reference_assertions: [
     {
-      "rcv": "RCV000077599",
-      "title": "NM_007294.4(BRCA1):c.5123C>A (p.Ala1708Glu) AND Breast-ovarian cancer, familial, susceptibility to, 1",
-      "clinical_significance": 0,
-      "review_status": 1
-    },
+      rcv: 'RCV000077599',
+      title:
+        'NM_007294.4(BRCA1):c.5123C>A (p.Ala1708Glu) AND Breast-ovarian cancer, familial, susceptibility to, 1',
+      clinical_significance: 0,
+      review_status: 1
+    }
   ]
 }
 
