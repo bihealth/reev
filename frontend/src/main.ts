7f81b2b1bb970d6b4388c883094f57f61cd1b18d
gromdimon <grom.dima.grom@gmail.com>
2023-11-06 14:18:25 +0100

    wip

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -18,18 +18,9 @@ async function bootstrap() {
   await registerPlugins(app)
 
   app.mount('#app')
-  await nextTick()
 
-  // @ts-ignore
+  await nextTick()
   window._paq.push(['trackPageView'])
-  // @ts-ignore
-  window._paq.push(['enableLinkTracking'])
-  const d = document,
-    g = d.createElement('script'),
-    s = d.getElementsByTagName('script')[0]
-  g.async = true
-  g.src = 'https://matomo.charite.de/matomo.js'
-  s.parentNode.insertBefore(g, s)
 }
 
 bootstrap()
