e966ae63486c5cdcd34f497e8e7bbefd5732c0b9
gromdimon <grom.dima.grom@gmail.com>
2023-11-13 18:25:50 +0100

    requirements

--- docs/conf.py
+++ docs/conf.py
@@ -3,11 +3,15 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# -- Setting up backend for documentation-------------------------------------
+
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../backend'))
+
+sys.path.insert(0, os.path.abspath("../backend"))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "REEV"
 copyright = "2023, Manuel Holtgrewe"
