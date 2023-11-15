596ea55eb09c634957d36e8c77520e3ffdf9cc27
gromdimon <grom.dima.grom@gmail.com>
2023-11-15 11:17:30 +0100

    fix: Close db connection on startup

--- backend/app/backend_pre_start.py
+++ backend/app/backend_pre_start.py
@@ -23,15 +23,15 @@ wait_seconds = 1
 )
 async def init():
     try:
-        db = SessionLocal()
-        # Try to create session to check if DB is awake
-        await db.execute(text("SELECT 1"))
-        # Ensure to run Alembic on startup
-        for key in ("DATABASE_URL", "SQLALCHEMY_DATABASE_URI"):
-            if key in os.environ:
-                os.environ[key] = os.environ[key].replace("+asyncpg", "")
-        alembicArgs = ["--raiseerr", "upgrade", "head"]
-        alembic.config.main(alembicArgs)
+        with SessionLocal() as db:
+            # Try to create session to check if DB is awake
+            await db.execute(text("SELECT 1"))
+            # Ensure to run Alembic on startup
+            for key in ("DATABASE_URL", "SQLALCHEMY_DATABASE_URI"):
+                if key in os.environ:
+                    os.environ[key] = os.environ[key].replace("+asyncpg", "")
+            alembicArgs = ["--raiseerr", "upgrade", "head"]
+            alembic.config.main(alembicArgs)
     except Exception as e:
         logger.error(e)
         raise e
