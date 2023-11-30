732cff6c3cca786af8f5b823303cd4d7e11fe84f
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-11-30 18:16:00 +0100

    feat: integrating sentry (#240) (#246)

--- frontend/src/plugins/index.ts
+++ frontend/src/plugins/index.ts
@@ -9,12 +9,13 @@ import type { App } from 'vue'
 
 import router from '../router'
 import pinia from '../stores'
-import setupMatomo from './matomo'
+import { setupMatomo } from './matomo'
+import { setupSentry } from './sentry'
 import vuetify from './vuetify'
 
 export async function registerPlugins(app: App) {
   app.use(vuetify).use(router).use(pinia)
 
-  // Initialize Matomo
-  await setupMatomo(app, router)
+  // Initialize Matomo and Sentry
+  await Promise.all([setupMatomo(app, router), setupSentry(app, router)])
 }
