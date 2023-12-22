90e8f7c31cdcd942b79309d4f86e9aa3e557a922
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-12-22 08:02:36 +0100

    build: make versions known to sentry

--- frontend/src/components/FooterDefault.vue
+++ frontend/src/components/FooterDefault.vue
@@ -1,23 +1,9 @@
-<script setup lang="ts">
-import { onMounted } from 'vue'
-
-import { useMiscStore } from '@/stores/misc'
-
-const miscStore = useMiscStore()
-
-onMounted(() => {
-  if (miscStore?.initialize) {
-    miscStore?.initialize()
-  }
-})
-</script>
-
 <template>
   <v-footer app>
     <small>
       Developed by BIH CUBI. For support and feedback, please contact
       <a href="mailto:cubi-helpdesk@bih-charite.de">cubi-helpdesk@bih-charite.de</a>. REEV Explains
-      and Evaluates Variants {{ miscStore?.appVersion }}.
+      and Evaluates Variants {{ __APP_VERSION__ }}.
       <router-link to="/info#terms-of-use">
         Terms of use
         <small> <v-icon>mdi-arrow-right-circle-outline</v-icon> </small> </router-link
--- frontend/src/components/PageHeader.vue
+++ frontend/src/components/PageHeader.vue
@@ -74,7 +74,7 @@ function toggleTheme() {
       </template>
       <v-spacer></v-spacer>
 
-      <v-btn variant="text" @click="toggleTheme">
+      <v-btn variant="text" @click="toggleTheme" title="Toggle between dark and light theme.">
         <v-icon>mdi-theme-light-dark</v-icon>
       </v-btn>
 
