b6be6524f8e9a78f0065ddc8e142309c98d7d562
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-12-22 09:15:34 +0100

    wip

--- utils/docker/entrypoint-celery-beat.sh
+++ utils/docker/entrypoint-celery-beat.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 
 set -x
-set -euo pipefail
 
 # Interpreted environment variables.
 #
@@ -13,6 +12,8 @@ if [[ $NO_WAIT_FOR -eq 0 ]]; then
     &>2 echo "Waiting for RabbitMQ..."
     &>2 /usr/local/bin/wait-for rabbitmq:5672 -t 60 -- echo "... RabbitMQ is ready"
 fi
+
+set -euo pipefail
 
 rm -f celerybeat.pid
 
--- utils/docker/entrypoint-celery-worker.sh
+++ utils/docker/entrypoint-celery-worker.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 
 set -x
-set -euo pipefail
 
 # Interpreted environment variables.
 #
@@ -21,6 +20,8 @@ if [[ $NO_WAIT_FOR -eq 0 ]]; then
     &>2 echo "Waiting for RabbitMQ..."
     &>2 /usr/local/bin/wait-for rabbitmq:5672 -t 60 -- echo "... RabbitMQ is ready"
 fi
+
+set -euo pipefail
 
 cd /home/reev && \
 PYTHONPATH=. python app/celery_pre_start.py
