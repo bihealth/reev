2c130c1d71d75ee3cd34f3ced99c2b2225ee1afd
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-12-22 08:11:24 +0100

    wip

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -1,3 +1,3 @@
 /// <reference types="vite/client" />
 
-declare const APP_VERSION: string;
+declare const __APP_VERSION__: string;
--- frontend/src/components/FooterDefault.vue
+++ frontend/src/components/FooterDefault.vue
@@ -1,9 +1,15 @@
+<script setup lang="ts">
+const rawAppVersion = __APP_VERSION__;
+const arr = rawAppVersion.split("@");
+const appVersion = arr[arr.length - 1];
+</script>
+
 <template>
   <v-footer app>
     <small>
       Developed by BIH CUBI. For support and feedback, please contact
       <a href="mailto:cubi-helpdesk@bih-charite.de">cubi-helpdesk@bih-charite.de</a>. REEV Explains
-      and Evaluates Variants {{ __APP_VERSION__ }}.
+      and Evaluates Variants {{ appVersion }}.
       <router-link to="/info#terms-of-use">
         Terms of use
         <small> <v-icon>mdi-arrow-right-circle-outline</v-icon> </small> </router-link
