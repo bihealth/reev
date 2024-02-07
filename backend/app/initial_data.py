98e6d917ddc93e008884e06977912b5aab99be59
Dzmitry Hramyka <grom.dima.grom@gmail.com>
2024-02-07 13:39:42 +0100

    fix: Fix initial superuser setup (#460) (#461)

--- backend/app/initial_data.py
+++ backend/app/initial_data.py
@@ -6,6 +6,8 @@ from app.db.init_db import init_db
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+logging.getLogger("passlib.handlers.bcrypt").setLevel(logging.ERROR)
+
 
 async def main() -> None:
     logger.info("Creating initial data")
