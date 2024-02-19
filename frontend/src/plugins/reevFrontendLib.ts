973dfb308baf611dc3d003b1b30aeeaf72ea46d4
gromdimon <grom.dima.grom@gmail.com>
2024-02-19 10:41:09 +0100

    chore: bump rfl integrate litvar

--- frontend/src/plugins/reevFrontendLib.ts
+++ frontend/src/plugins/reevFrontendLib.ts
@@ -6,6 +6,7 @@ export function setupBackendUrls() {
   urlConfig.baseUrlViguno = '/internal/proxy/viguno'
   urlConfig.baseUrlNginx = '/internal/proxy/nginx'
   urlConfig.baseUrlPubtator = '/internal/remote/pubtator3-api'
+  urlConfig.baseUrlLitVar = '/internal/remote/litvar'
   urlConfig.baseUrlCadaPrio = '/internal/proxy/cada-prio'
   urlConfig.baseUrlDotty = '/internal/proxy/dotty'
   urlConfig.baseUrlVariantValidator = '/internal/remote/variantvalidator'
